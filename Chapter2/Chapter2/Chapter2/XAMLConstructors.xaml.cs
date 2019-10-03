b11a6aa659c11a8e278f06d6e7b4cb48ce55945a
unknown <tranvanquang2504@gmail.com>
2019-10-03 22:56:57 +0700

    Chapter 1 and Chapter 2

---
+++ Chapter2/Chapter2/Chapter2/XAMLConstructors.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chapter2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XAMLConstructors : ContentPage
+    {
+        public XAMLConstructors()
+        {
+            InitializeComponent();
+        }
+    }
+}
